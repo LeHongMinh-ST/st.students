d9831787f618724d195736b3297e3dec784c0e01
minhlh <minhlh@haposoft.com>
2025-03-20 14:55:09 +0700

    fix: fix provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Enums\Role;
 use App\View\Components\Layouts\AdminLayout;
 use App\View\Components\Layouts\AuthLayout;
 use App\View\Components\Layouts\ClientLayout;
 use App\View\Components\Table\TableEmpty;
 use App\View\Components\User\RoleBadge;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Passport\Passport;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,15 +32,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        LogViewer::auth(fn ($request) => $request->user()
+                && in_array($request->user()->role, [
+                    Role::SuperAdmin,
+                ]));
+
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event): void {
             $event->extendSocialite('azure', \SocialiteProviders\Azure\Provider::class);
         });
 
-        Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
-        Passport::hashClientSecrets();
-        Passport::tokensExpireIn(now()->addDays(15));
-        Passport::refreshTokensExpireIn(now()->addDays(30));
-        Passport::personalAccessTokensExpireIn(now()->addDays(15));
+
 
         Blade::component('auth-layout', AuthLayout::class);
         Blade::component('admin-layout', AdminLayout::class);
