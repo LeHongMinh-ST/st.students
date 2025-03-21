10673b3e37acb87b4792eb4a1a6d87d622e1eb18
minhlh <minhlh@haposoft.com>
2025-03-21 10:17:44 +0700

    fix: never used avatar

--- database/migrations/2025_03_19_150018_create_approve_student_updates_table.php
+++ database/migrations/2025_03_19_150018_create_approve_student_updates_table.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use App\Enums\StudentInfoUpdateStatus;
+use App\Enums\StudentUpdateStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,7 +17,7 @@ return new class () extends Migration {
             $table->id();
             $table->string('approveable_type');
             $table->bigInteger('approveable_id');
-            $table->string('status')->default(StudentInfoUpdateStatus::Pending->value);
+            $table->string('status')->default(StudentUpdateStatus::Pending->value);
             $table->unsignedBigInteger('student_info_updates_id')->nullable()->index();
             $table->text('note')->nullable();
             $table->timestamps();
