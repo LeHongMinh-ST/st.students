81d1ffcc81626b16dc2b4e59b05fd785876b5e5d
minhlh <minhlh@haposoft.com>
2025-03-27 11:26:21 +0700

    feat: import student

---
+++ app/Imports/StudentGradutionImport.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Imports;
+
+class StudentGradutionImport extends StudentImport
+{
+    protected function handleImport(array $row): void
+    {
+
+    }
+}
---
+++ app/Imports/StudentWarningImport.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Imports;
+
+class StudentWarningImport extends StudentImport
+{
+    protected function handleImport(array $row): void
+    {
+
+    }
+}
