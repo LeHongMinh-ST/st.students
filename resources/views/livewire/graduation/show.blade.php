35bffedcc3c6d2dd51f0b8918d30198dedbf15c2
minhlh <minhlh@haposoft.com>
2025-06-24 10:05:19 +0700

    feat: sync data

--- resources/views/livewire/graduation/show.blade.php
+++ resources/views/livewire/graduation/show.blade.php
@@ -122,7 +122,7 @@
                                 <td width="25%">{{ $item->pivot->email ?: $item->email }}</td>
                                 <td width="15%">{{ number_format($item->pivot->gpa, 2) }}</td>
                                 <td width="20%">
-                                    <x-rank-graduate-badge :rank="\App\Enums\RankGraduate::from($item->pivot->rank)" />
+                                    {{ $item->pivot->rank }}
                                 </td>
                             </tr>
                         @empty
